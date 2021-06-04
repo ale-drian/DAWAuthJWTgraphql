e3d6c33f788be938f0d911d893a70a6a49259a0d
unknown <alejandraadriantejada@gmail.com>
2021-06-04 02:20:05 -0500

    Paso: Integrate with Graphql server and fix some errors in yarn dev

--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 //Cambio de config.json a config.js
-const config = require(__dirname + '/../../config/config.js')[env]
+const config = require(__dirname + '/../config/config.js')[env]
 const db = {};
 
 let sequelize;
