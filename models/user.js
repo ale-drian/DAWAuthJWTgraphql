84ce85467e2ee8d96e94e05ab3e94b2237af14d1
unknown <alejandraadriantejada@gmail.com>
2021-06-04 01:44:50 -0500

    Paso: Database setup

---
+++ models/user.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+        username: {
+            type: DataTypes.STRING,
+        },
+        email: {
+            type: DataTypes.STRING,
+        },
+        password: {
+            type: DataTypes.STRING,
+        }
+    }, {});
+    return User;
+};
+
+/* 'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class User extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+/*
+static associate(models) {
+    // define association here
+}
+};
+User.init({
+    username: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING
+}, {
+    sequelize,
+    modelName: 'User',
+});
+return User;
+}; */
